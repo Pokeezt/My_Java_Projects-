8f9bb3726dd3c9d2e2d2fc8bc76ee51973b3f5d8
Kauê Brás <100492131+Pokeezt@users.noreply.github.com>
2025-03-27 11:45:26 -0300

    Create MatematicaBasica.java

---
+++ MatematicaBasica.java
@@ -0,0 +1,62 @@
+package qlqrcoisa;
+
+public class MatematicaBasica {
+	  public static void main(String[] args) {
+	    
+	    int x = 2;
+	    int y = 10;
+	    int z = 15;
+	    // ou pode ser assim
+	    int xx = 5, yy = 17, zz = 32;
+	    
+	    // Adição (+)
+	    int soma = x + y + z;
+	    System.out.println("Adição x, y e z:" + soma);
+
+	    int soma2 = xx + yy + zz;
+	    System.out.println("Adição xx, yy, e zz:" + soma2);
+	    
+	    //Subtração (-)
+	    int soma3 = x - y - z;
+	    System.out.println("Subtração x, y e z:" + soma3);
+
+	    // Multiplicação
+	    int soma4 = y * x;
+	    System.out.println("Multiplição y * x" + soma4);
+	    
+	    // Divisão
+	    int soma5 = x / z;
+	    System.out.println("Divisão x / z" + soma5);
+	    
+	    // Uma lista de todos os operadores de atribuição:
+	    // Igual = 
+	    int a = 5;
+	    System.out.println("Igual: " +a);
+
+	    // Soma +=
+	    int b = 5;
+	    b += 3; // b = b + 3;
+	    System.out.println("Soma: " + b);
+
+	    // Menos -=
+	    int c = 5;
+	    c -= 3; // c = c - 3;
+	    System.out.println("Menos: " + c);
+
+	    // Multiplicação
+	    int d = 5;
+	    d *= 3; // d = d * 
+	    System.out.println("Mutiplicação: " + d);
+	    
+	    // Divisão
+	    int f = 10;
+	    f /= 2;
+	    System.out.println("Divisão: " + f);
+	    
+	    
+	    
+	    
+	    
+	    
+	  }
+	}

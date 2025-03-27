d38e2acbee3385ed0ff14d85671ea1030dc3324d
Kauê Brás <100492131+Pokeezt@users.noreply.github.com>
2025-03-27 10:50:30 -0300

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    
+    //ou, pode ser outro tipo assim:
+    String hello = "Hello World";
+    System.out.println(hello);
+    
+    // Mais tem outro tipo tambem:
+    String h = "Hello";
+    String w = "World";
+    System.out.println(h+" "+w);
+
+    //Todo ambos mostra "Hello World".
+  }
+}
